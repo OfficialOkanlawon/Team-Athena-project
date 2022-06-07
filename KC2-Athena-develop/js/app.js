cb42466af0a814421ad275a516094723698b2fa2
OfficialOkanlawon <okanlawonimmanuel@gmail.com>
2022-06-07 18:50:11 +0100

    ch: Team Athena project

---
+++ KC2-Athena-develop/js/app.js
@@ -0,0 +1,17 @@
+document.querySelector(".header-name").innerHTML = "CSUF"
+document.querySelector(".header-name").style.color = "#4997d0"
+
+document.querySelector(".bold-name").innerHTML = "CSUF"
+document.querySelector(".bold-name").style.color = "#4997d0"
+
+document.querySelector(".pa-name").innerHTML = "Algo"
+document.querySelector(".pa-name").style.color = "#6a5acd"
+
+document.querySelector(".pat-name").innerHTML = "Create"
+document.querySelector(".pat-name").style.color = "#ff355e"
+
+document.querySelector(".path-name").innerHTML = "Dev"
+document.querySelector(".path-name").style.color = "#1e90ff"
+
+document.querySelector(".sign").innerHTML = "+"
+document.querySelector(".sign").style.color = "#4997d0"
